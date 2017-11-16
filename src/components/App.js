2f4967fcc680c6546839cf7276650de71257235f
AzgatG <azgatherzjob@gmail.com>
2017-11-16 14:09:06 +0300

    add css loader  -add react-select

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Select from 'react-select';
+
+import 'react-select/dist/react-select.css';
+
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
 
@@ -7,12 +11,30 @@ export default class App extends React.Component {
   static propTypes = {
   };
 
+  state = {
+  	selection :null
+  }
+
   render() {
+	  const options = this.props.articles.map(article => ({
+	  	label: article.title,
+	  	value: article.id
+	  }))
+
     return (
       <div>
 					<UserForm />
+					<Select 
+						name="form-field-name"
+					  value={this.state.selection}
+						options={options}
+						onChange={this.changeSelection}
+						multi
+					/>
 					<ArticleList articles={this.props.articles} />
       </div>
     );
   }
+
+  changeSelection = selection => this.setState({selection})
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
+
+import './style.css'
+
 import App from './components/App'
 import {articles} from './fixtures'
+
 
 render(<App articles={articles} />, document.getElementById('container'));
