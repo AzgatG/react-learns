e008d73f773276c67325e067a8f908bf80a5ef49
AzgatG <azgatherzjob@gmail.com>
2017-11-27 11:59:24 +0300

    immutable articles

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,39 +1,33 @@
 import {normalizedArticles as defaultArticles} from '../fixtures';
 import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES} from '../constants';
-import {arrToMap} from '../helpers'
-import {Map} from 'immutable'
+import {arrToMap} from '../helpers.js'
+import {Map, Record} from 'immutable'
 
+
+const ArticleRecord = Record({
+  text: undefined,
+  title: undefined,
+  id: undefined,
+  comments: [],
+})
 
 const defaultState = new Map({})
 
-export default (articlesState = {}, action) => {
+export default (articlesState = defaultState, action) => {
   const {type, payload, responce, randomId} = action;
 
   switch (type) {
     case DELETE_ARTICLE: 
-      const tmpState = {...articlesState}
-      delete tmpState[payload.id]
-      return tmpState
+      return articlesState.delete(payload.id)
 
     case ADD_COMMENT:
       const {articleId} = payload;
-      const article = articlesState[articleId];
-      ////////////////////////////////////////////////
-      // Не работает потому что вложенность большая //
-      // articlesState[articleId].comments.push(randomId)
-      // return articlesState
-      // в комментах работате
-      ////////////////////////////////////////////////
-      return {
-        ...articlesState,
-        [articleId]: {
-          ...article,
-          comments: (article.comments || []).concat(randomId)
-        }
-      }
+      return articlesState.updateIn([articleId, 'comments'], comments => {
+        return comments.concat(randomId)
+      })
 
     case LOAD_ALL_ARTICLES:
-      return arrToMap(responce)
+      return arrToMap(responce, ArticleRecord)
 
   }
 
